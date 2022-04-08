cac0137311e2ba046083e26dd4d695d38062bef6
Andres Fonts Santana <afonts@hp.com>
2022-04-08 17:29:27 +0200

    Update codigo

--- py/gen_boardgame_dataset.py
+++ py/gen_boardgame_dataset.py
@@ -12,29 +12,47 @@ import csv
 from get_ratings import get_ratings
 
 # Función ficticia get_info()
-def get_info(url):
-    print(url)
-    time.sleep(0.02)
+# def get_info(url):
+#     print(url)
+#     time.sleep(0.02)
     
-    category = ['Jugadores', 'Duración', 'Edad', 'Dureza', 'Precio', 'Género',
-                'Editorial', 'Diseñador/a', 'Total', 'Aspecto / Componentes',
-                'Interacción', 'Variabilidad', 'Originalidad', 'Mecánicas',
-                'Conclusión']
+#     category = ['Jugadores', 'Duración', 'Edad', 'Dureza', 'Precio', 'Género',
+#                 'Editorial', 'Diseñador/a', 'Total', 'Aspecto / Componentes',
+#                 'Interacción', 'Variabilidad', 'Originalidad', 'Mecánicas',
+#                 'Conclusión']
     
-    values = []
-    for k in range(len(category)):
-        values.append(rnd.randint(1,100))
+#     values = []
+#     for k in range(len(category)):
+#         values.append(rnd.randint(1,100))
     
-    id_rnd = rnd.randint(1,10000)
-    name = 'Juego_' + str(id_rnd)
+#     id_rnd = rnd.randint(1,10000)
+#     name = 'Juego_' + str(id_rnd)
     
-    d = defaultdict(dict)
-    for c,v in zip(category,values):
-        d[name][c]=v
+#     d = defaultdict(dict)
+#     for c,v in zip(category,values):
+#         d[name][c]=v
     
-    return d
+#     return d
 
 def info2list(dict_review, csv_header):
+    '''
+    
+
+    Parameters
+    ----------
+    dict_review : TYPE
+        DESCRIPTION.
+    csv_header : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    list_review : TYPE
+        DESCRIPTION.
+    exception : TYPE
+        DESCRIPTION.
+
+    '''
     
     list_review = [0] * len(csv_header)
     
@@ -51,11 +69,15 @@ def info2list(dict_review, csv_header):
                 'Edad mínima recomendada': 'edad',
                 'Precio':                  'precio',
                 'PVP Recomendado':         'precio',
+                'PVP aproximado':          'precio',
+                'PVP':                     'precio',
+                'Precio Recomendado':      'precio',
                 'Género':                  'genero',
                 'Genero/mecánicas':        'genero',
                 'Genero / Mecánicas':      'genero',
                 'Género / Mecánicas':      'genero',
                 'Editorial':               'editorial',
+                'Editorial/Distribuidora': 'editorial',
                 'Diseñador/a':             'diseño',
                 'Autor/a':                 'diseño',
                 'Total':                   'val_glob',
@@ -66,12 +88,16 @@ def info2list(dict_review, csv_header):
                 'Rejugabilidad':           'val_rej',
                 'Originalidad':            'val_org', 
                 'Mecánicas':               'val_mec',
+                'Calidad de mecánicas':    'val_mec',
                 'Calidad de Mecánicas':    'val_mec',
+                'Calidad Mecánica':        'val_mec',
                 'Conclusion':              'val_cual'}
      
     # Capturo el nombre del juego
     name = list(dict_review.keys())[0]    
     list_review[csv_header.index('nombre')] = name
+    
+    exception = []
     
     values = list(dict_review.values())[0]
     for key in values.keys():
@@ -80,12 +106,14 @@ def info2list(dict_review, csv_header):
             list_review[idx] = values[key]
         except:
             print(key, ':', values[key])       
+            exception.append([key, values[key]])
     
-    return list_review
+    return list_review, exception
 
 # Obtener la lista de enlaces a reseñas y guardar en list
-#filename = "mishigeek_reviews.csv"
-filename = "mishigeek_reviews_test.csv"
+filename = "mishigeek_reviews.csv"
+#filename = "mishigeek_reviews_test.csv"
+
 url_list = []
 
 with open(filename, 'r') as f:
@@ -97,32 +125,19 @@ csv_header = ['nombre', 'n_jug', 'fecha', 'duracion', 'dureza', 'edad', 'precio'
               'genero', 'editorial', 'diseño', 'val_asp', 'val_inter', 'val_div',
               'val_var', 'val_rej', 'val_org', 'val_mec', 'val_glob', 'val_cual']
 
-# csv_map  = {'n_jug':'Jugadores',
-#             'duracion':'Duración',
-#             'dureza':'Dureza',
-#             'edad':'Edad',
-#             'precio':'Precio',
-#             'genero': 'Género',
-#             'editorial':'Editorial',
-#             'diseño': 'Diseñador/a',
-#             'val_glob':'Total',
-#             'val_asp':'Aspecto / Componentes',
-#             'val_inter':'Interacción',
-#             'val_var':'Variabilidad',
-#             'val_org':'Originalidad',
-#             'val_mec':'Mecánicas',
-#             'val_cual':'Conclusión'}
-
 # Iterar sobre review_list
 dataset = []
+exceptions = []
 
 for url in url_list:
     try:
-        dataset.append(info2list(get_ratings(url), csv_header))
+        list_review, exception = info2list(get_ratings(url), csv_header)
+        dataset.append(list_review)
+        exceptions.append(exception)
     except:
         print('No se ha analizado: ', url[0])
 
-# # Escribir el dataset a un archivo .csv
+# Escribir el dataset a un archivo .csv
 with open('boardgame_ranking.csv', 'w', newline='', encoding='utf-8') as f:
     writer = csv.writer(f, delimiter=',')
     writer.writerow(csv_header)
@@ -131,6 +146,7 @@ with open('boardgame_ranking.csv', 'w', newline='', encoding='utf-8') as f:
         writer.writerow(row)
 
 print(dataset)
+print(exceptions)
 
 
 
