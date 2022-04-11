012c99972c07a25d41069978a4248641eb7f23ee
ignaciosuchUOC <isuch@uoc.edu>
2022-04-11 11:19:17 +0200

    Corrección
    Lo que añadí en el archivo get_ratings_functions, lo he añadido en el  get_ratings

--- py/get_ratings_function.py
+++ py/get_ratings_function.py
@@ -11,7 +11,6 @@ import re
 from bs4 import BeautifulSoup
 import os
 from collections import defaultdict
-import itertools
 
 # Urls que voy a usar
 review = ["https://mishigeek.com/its-a-wonderful-kingdom-resena/", 'aprobado']
@@ -73,30 +72,6 @@ if (page.ok==True):
     for c,v in zip(category,value):
         d[nombre][c]=v    
     
-    def split_before_number(text):
-        """Split text into 2 parts: before the first digit and the rest."""
-        def not_digit(c):
-            """Return True if character c is not a digit."""
-            return not c.isdigit()
-        before = ''.join(itertools.takewhile(not_digit, text))
-        after = ''.join(itertools.dropwhile(not_digit, text))
-        return before, after
-    def split_key_and_value(key, value):
-        if not key.startswith("Nota"):
-            yield key, value
-            return
-    
-        key1, value2 = split_before_number(key)
-        yield key1, value
-        yield "N. Votes", value2
-    def transform(dict_object):
-        """Split some specific keys and values and form a new dict."""
-        new_dict_object = {}
-        for original_key, original_value in dict_object.items():
-            for key, value in split_key_and_value(original_key, original_value):
-                new_dict_object[key] = value
-        return new_dict_object
-    d = {key: transform(value) for key, value in d.items()}
     
     print(d)
 
